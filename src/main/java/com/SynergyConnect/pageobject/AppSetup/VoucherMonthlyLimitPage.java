897dffea198684784b1cd5960dcbb4bff5aee8f7
Abhijeet Maske <abhijeet.msk@gmail.com>
2024-08-09 14:10:59 +0530

    Added Locators for Elements on works/AppSetup/VoucherMonthlyLimit Page

--- src/main/java/com/SynergyConnect/pageobject/AppSetup/VoucherMonthlyLimitPage.java
+++ src/main/java/com/SynergyConnect/pageobject/AppSetup/VoucherMonthlyLimitPage.java
@@ -1,5 +1,82 @@
 package com.SynergyConnect.pageobject.AppSetup;
 
-public class VoucherMonthlyLimitPage {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.SynergyConnect.utilities.AlertUtils;
+import com.SynergyConnect.utilities.ElementInteractionUtils;
+import com.SynergyConnect.utilities.ReadConfig;
+
+public class VoucherMonthlyLimitPage {
+	WebDriver driver;
+	ElementInteractionUtils EI;
+	AlertUtils AU;
+	private static final Logger logger = LogManager.getLogger(VoucherMonthlyLimitPage.class);
+	ReadConfig config = new ReadConfig();
+
+	public VoucherMonthlyLimitPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		EI = new ElementInteractionUtils(driver);
+		AU = new AlertUtils(driver);
+	}
+
+	// identify WebElements
+	@FindBy(xpath = "//span[contains(text(),'App Setup')]")
+	private WebElement ddlAppSetup;
+
+	@FindBy(xpath = "//span[contains(text(),'Voucher Monthly Limit')]")
+	private WebElement ddlCompanyType;
+
+	@FindBy(xpath = "//*[@id=\"collapseBtn\"]")
+	private WebElement btnVoucherMonthlyLimitAdd;
+	
+	@FindBy(xpath = "//*[@id=\"selectRoll\"]")
+	private WebElement ddlRole;
+
+	@FindBy(xpath = "//*[@id=\"selectUser\"]")
+	private WebElement ddlUser;
+	
+	@FindBy(xpath = "//*[@id=\"voucherType\"]")
+	private WebElement ddlVoucherType;
+	
+	@FindBy(xpath = "//*[@id=\"monthlyAmount\"]")
+	private WebElement txtAmountLimit;
+	
+	@FindBy(xpath = "//*[@id=\"addNewRow\"]")
+	private WebElement btnAddVoucherMonthlyLimit;
+
+	@FindBy(xpath = "//*[@id=\"voucherMonthlyLimitForm\"]/div/div[5]/button[1]")
+	private WebElement btnCancel;
+	
+	@FindBy(xpath = "//*[@id=\"activeBtn\"]")
+	private WebElement btnActiveTab;
+
+	@FindBy(xpath = "//*[@id=\"activeDataTable\"]")
+	private WebElement tblVoucherMonthlyLimitActive;
+
+	@FindBy(xpath = "//*[@id=\"bEdit\"]")
+	private WebElement btnEdit;
+
+	@FindBy(xpath = "//*[@id=\"bElim\"]")
+	private WebElement btnDelete;
+
+	@FindBy(xpath = "//*[@id=\"activeDataTable_next\"]/a")
+	private WebElement btnVoucherMonthlyLimitActiveNext;
+
+	@FindBy(xpath = "//*[@id=\"nonActiveBtn\"]")
+	private WebElement btnInactiveTab;
+
+	@FindBy(xpath = "//*[@id=\"DyanmicTable01_wrapper\"]")
+	private WebElement tblVoucherMonthlyLimitInactive;
+
+	@FindBy(xpath = "//*[@id=\"bElim\"]")
+	private WebElement btnActive;
+
+	@FindBy(xpath = "//*[@id=\"DyanmicTable01_next\"]")
+	private WebElement btnVoucherMonthlyLimitInactiveNext;
 }
